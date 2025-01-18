bc2923d4fbe4d896a7812d510c2d8ba61d471071
Alexandru Pistol <alexandrupi@payoneer.com>
2025-01-18 05:02:24 +0200

    refactor menu item

---
+++ components/types/Order.tsx
@@ -0,0 +1,12 @@
+export interface OrderItem {
+    id: number;
+    name: string;
+    qty: number
+
+    // TODO this needs refactor and replace menuItems
+    // title
+    // description
+    // imageSrc
+    // nextId
+    // menuName
+}
