2715cb5b01adbd04068a6c3ae2bb84cb564b4ae4
Alexandru Pistol <alexandrupi@payoneer.com>
2025-01-18 05:00:12 +0200

    set analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type {Metadata} from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Script from 'next/script';
+import {EventProvider} from "@/components/context";
 
 const geistSans = localFont({
     src: "./fonts/GeistVF.woff",
@@ -29,11 +31,26 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+        <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-PF87JCCBTG"
+            strategy="afterInteractive" // Ensures the script loads after the page is interactive
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-PF87JCCBTG');
+        `}
+        </Script>
         <body
             className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-        {children}
+        <EventProvider>
+            {children}
+        </EventProvider>
         </body>
+
         </html>
     );
 }
