241fc012d4a949d8ff3ba56fed6dd09b6e65217e
Alexandru Pistol <alexandrupi@payoneer.com>
2025-01-18 20:13:48 +0200

    refactor menu

--- components/sections/Menu.tsx
+++ components/sections/Menu.tsx
@@ -2,21 +2,42 @@ import React from 'react';
 import Image from "next/image";
 import {useEventContext, menuItems} from "@/components/context";
 import smoothScrollToSection from "@/components/utils/smoothScrollToSection";
+import InfoIcon from "@mui/icons-material/Info";
+import getIsMobile from "@/components/utils/isMobile";
+import {IconButton, Tooltip} from "@mui/material";
 
 function Menu() {
-    const {addOne} = useEventContext();
 
+    const {addOne} = useEventContext();
+    const isMobile = getIsMobile();
+
+    const getDrinkDescription = (index: number) => {
+        return isMobile ? (
+            <div className={"flex justify-center font-grotesk m-10"}>
+                <p>{menuItems[index].shortDescription} </p>
+                <Tooltip title={menuItems[index].description}>
+                    <IconButton>
+                        <InfoIcon className={"info-icon"}/>
+                    </IconButton>
+                </Tooltip>
+            </div>
+        ) : (
+            <p className={"font-grotesk m-10"}>{menuItems[index].description} </p>
+        )
+    }
 
     return (
         <div className={"flex flex-col relative"}>
-            <h2 id={"menu"} className={"text-center text-2xl lg:text-5xl font-grotesk"}>MENIURI</h2>
+            <h2 id={"menu"} className={"text-center text-2xl lg:text-5xl font-grotesk"}>
+                Cu ce ti-ai rasfata invitatii?
+            </h2>
             {menuItems.map((item, index) => (
                 <div key={item.id}
-                     id={String(item.id)}
-                     className={"h-full lg:h-[80vh] flex flex-col lg:flex-row justify-center align-middle"}>
-                    <div className={"lg:w-1/3 flex flex-col justify-center mb-10 lg:mb-0"}>
-                        <h3 className={"text-4xl text-center font-grotesk font-bold"}>{item.title}</h3>
-                        <p className={"font-grotesk m-10"}>{item.description}</p>
+                     id={menuItems[index].currentId}
+                     className={"h-screen flex flex-col lg:flex-row justify-center align-middle"}>
+                    <div className={"lg:w-1/3 flex flex-col justify-center lg:mb-0"}>
+                        <h3 className={"text-4xl text-center font-grotesk font-bold"}>{menuItems[index].title}</h3>
+                        {getDrinkDescription(index)}
                     </div>
                     <Image
                         src={item.imageSrc}
@@ -28,24 +49,25 @@ function Menu() {
                     />
                     <div className={"w-full lg:w-1/3 flex flex-col justify-center align-middle"}>
                         <button
-                            className={"text-center custom-button w-40 mx-auto"}
+                            className={"text-center custom-button w-72 mx-auto"}
                             onClick={() => {
                                 const selectedItem = menuItems[index];
                                 addOne({
-                                    id:selectedItem.id,
+                                    id: selectedItem.id,
                                     name: selectedItem.title,
                                     qty: 1
                                 });
+                                smoothScrollToSection(menuItems[index].nextId)
                             }}
                         >
-                            Imi place
+                            As incerca
                         </button>
-                        {item.nextId && (
+                        {menuItems[index].nextId && (
                             <button
-                                className={"text-center custom-button-oulined w-40 mx-auto"}
-                                onClick={() => smoothScrollToSection(item.nextId)}
+                                className={"text-center custom-button-oulined w-72 mx-auto"}
+                                onClick={() => smoothScrollToSection(menuItems[index].nextId)}
                             >
-                                Urmatorul
+                                Hai sa mai vedem
                             </button>
                         )}
                     </div>
