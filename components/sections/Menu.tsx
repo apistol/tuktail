e78368b16e4ec25af69177de0764a060bacc6934
Alexandru Pistol <alexandrupi@payoneer.com>
2025-01-19 18:37:15 +0200

    fix tooltips on mobile

--- components/sections/Menu.tsx
+++ components/sections/Menu.tsx
@@ -1,30 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from "next/image";
-import {useEventContext, menuItems} from "@/components/context";
+import { useEventContext, menuItems } from "@/components/context";
 import smoothScrollToSection from "@/components/utils/smoothScrollToSection";
 import InfoIcon from "@mui/icons-material/Info";
 import getIsMobile from "@/components/utils/isMobile";
-import {IconButton, Tooltip} from "@mui/material";
+import { IconButton, Tooltip } from "@mui/material";
 
 function Menu() {
-
-    const {addOne} = useEventContext();
+    const { addOne } = useEventContext();
     const isMobile = getIsMobile();
+    const [tooltipOpen, setTooltipOpen] = useState<{ [key: number]: boolean }>({});
 
-    const getDrinkDescription = (index: number) => {
+    const handleTooltipToggle = (index:number) => {
+        setTooltipOpen((prev) => ({
+            ...prev,
+            [index]: true,
+        }));
+
+        // Automatically close the tooltip after 5 seconds
+        setTimeout(() => {
+            setTooltipOpen((prev) => ({
+                ...prev,
+                [index]: false,
+            }));
+        }, 5000);
+    };
+
+    const getDrinkDescription = (index:number) => {
+        const description = menuItems[index].description;
+        const shortDescription = menuItems[index].shortDescription;
+
         return isMobile ? (
-            <div className={"flex justify-center font-grotesk m-10"}>
-                <p>{menuItems[index].shortDescription} </p>
-                <Tooltip title={menuItems[index].description}>
-                    <IconButton>
-                        <InfoIcon className={"info-icon"}/>
+            <div className={"flex flex-row items-center font-grotesk m-10 z-10"}>
+                <p>{shortDescription}</p>
+                <Tooltip
+                    title={description}
+                    open={tooltipOpen[index] || false}
+                    onClose={() => setTooltipOpen((prev) => ({
+                        ...prev,
+                        [index]: false,
+                    }))}
+                    disableFocusListener
+                    disableHoverListener
+                    disableTouchListener
+                >
+                    <IconButton onClick={() => handleTooltipToggle(index)}>
+                        <InfoIcon className={"info-icon"} />
                     </IconButton>
                 </Tooltip>
             </div>
         ) : (
-            <p className={"font-grotesk m-10"}>{menuItems[index].description} </p>
-        )
-    }
+            <p className={"font-grotesk m-10"}>{description}</p>
+        );
+    };
 
     return (
         <div className={"flex flex-col relative"}>
@@ -32,11 +60,15 @@ function Menu() {
                 Cu ce ti-ai rasfata invitatii?
             </h2>
             {menuItems.map((item, index) => (
-                <div key={item.id}
-                     id={menuItems[index].currentId}
-                     className={"h-screen flex flex-col lg:flex-row justify-center align-middle"}>
+                <div
+                    key={item.id}
+                    id={menuItems[index].currentId}
+                    className={"h-screen flex flex-col lg:flex-row justify-center align-middle"}
+                >
                     <div className={"lg:w-1/3 flex flex-col justify-center lg:mb-0"}>
-                        <h3 className={"text-4xl text-center font-grotesk font-bold"}>{menuItems[index].title}</h3>
+                        <h3 className={"text-4xl text-center font-grotesk font-bold"}>
+                            {menuItems[index].title}
+                        </h3>
                         {getDrinkDescription(index)}
                     </div>
                     <Image
@@ -45,7 +77,7 @@ function Menu() {
                         className={"w-2/5 lg:w-1/3 lg:p-20 mx-auto mb-10 lg:mb-0"}
                         width={364}
                         height={577}
-                        style={{objectFit: 'contain'}}
+                        style={{ objectFit: 'contain' }}
                     />
                     <div className={"w-full lg:w-1/3 flex flex-col justify-center align-middle"}>
                         <button
@@ -55,9 +87,9 @@ function Menu() {
                                 addOne({
                                     id: selectedItem.id,
                                     name: selectedItem.title,
-                                    qty: 1
+                                    qty: 1,
                                 });
-                                smoothScrollToSection(menuItems[index].nextId)
+                                smoothScrollToSection(menuItems[index].nextId);
                             }}
                         >
                             As incerca
